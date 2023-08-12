1f8b92ca24c4422558e2bc8d205b6635b4ac3738
Артур Сагадеев <arturik_sagadeev@mail.ru>
2023-08-12 22:54:10 +0500

    Task5

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -15,6 +15,7 @@ internal class Program
 		}
 		return result;
 	}
+
 	private static string evenOrOdd(char[] chars)
 	{
 		int lenString = chars.Length;
@@ -41,6 +42,7 @@ internal class Program
 		}
 		return result;
 	}
+
 	private static void countAllChars(string inputString)
 	{
 		char[] chars = inputString.ToCharArray();
@@ -61,6 +63,107 @@ internal class Program
 			Console.WriteLine($"Буква: {c.Key} встречается: {c.Value} раз.");
 		}
 	}
+
+	private static void maxSubString(string inputString)
+	{
+		char[] chars = inputString.ToCharArray();
+		string alphabet = "aeoiuy";
+		int first = -1;
+		int second = -1;
+		for (int i = 0; i < chars.Length; i++)
+		{
+			if (alphabet.Contains(chars[i]))
+			{
+				if (first == -1)
+				{
+					first = i;
+				}
+				else
+				{
+					second = i;
+				}
+			}
+		}
+		if (first == -1 || second == -1)
+		{
+			Console.WriteLine("В данной строке менее двух гласных букв");
+		}
+		else
+		{
+			char[] result = new char[second - first + 1];
+			Array.Copy(chars, first, result, 0, second - first + 1);
+			Console.WriteLine(new string(result));
+		}
+	}
+
+	private static int Partition(char[] A, int l, int r)
+	{
+		int barrier = A[(l + r) / 2];
+		int i = l;
+		int j = r;
+
+		while (i <= j)
+		{
+			while (A[i] < barrier) i++;
+			while (A[j] > barrier) j--;
+			if (i >= j) break;
+
+			int m = i++;
+			int u = j--;
+
+			char tmp = A[m];
+			A[m] = A[u];
+			A[u] = tmp;
+		}
+
+		return j;
+
+	}
+
+	private static void QuickSort(char[] arr, int l, int r)
+	{
+		if (l < r)
+		{
+			int q = Partition(arr, l, r);
+			QuickSort(arr, l, q);
+			QuickSort(arr, q + 1, r);
+		}
+	}
+
+	static void HeapSort(char[] arr)
+	{
+		int n = arr.Length;
+		for (int i = n / 2 - 1; i >= 0; i--)
+		{
+			Heapify(arr, n, i);
+		}
+		for (int i = n - 1; i >= 0; i--)
+		{
+			char temp = arr[0];
+			arr[0] = arr[i];
+			arr[i] = temp;
+			Heapify(arr, i, 0);
+		}
+	}
+
+	static void Heapify(char[] arr, int n, int i)
+	{
+		int largest = i;
+		int l = 2 * i + 1;
+		int r = 2 * i + 2;
+		if (l < n && arr[l] > arr[largest])
+			largest = l;
+		if (r < n && arr[r] > arr[largest])
+			largest = r;
+		if (largest != i)
+		{
+			char swap = arr[i];
+			arr[i] = arr[largest];
+			arr[largest] = swap;
+			Heapify(arr, n, largest);
+		}
+	}
+
 	private static void Main(string[] args)
 	{
 		string inputString = Console.ReadLine();
@@ -68,8 +171,20 @@ internal class Program
 		string result = englishLowRegister(chars);
 		if (String.IsNullOrEmpty(result))
 		{
-			countAllChars(evenOrOdd(chars));
-		}
+            Console.WriteLine("1)Обработанная строка:");
+            string finalString = evenOrOdd(chars);
+
+            Console.WriteLine("2)Информация о том, сколько раз входил в обработанную строку каждый символ:");
+            countAllChars(finalString);
+
+            Console.WriteLine("3)Самая длинная подстрока начинающаяся и заканчивающаяся на гласную:");
+            maxSubString(finalString);
+
+            Console.WriteLine("4)Отсортированная обработанная строка при помощи пирамидального метода сотрировки:");
+            char[] sortedArray = finalString.ToCharArray();
+			QuickSort(sortedArray);
+            Console.WriteLine(new string(sortedArray));
+        }
 		else
 		{
 			Console.WriteLine("Ошибка((( в строке находятся вот такие недопустимые символы: " + result);
